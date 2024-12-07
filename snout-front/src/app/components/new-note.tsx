875446589275890f2ebebc1670c8691938693465
l-furquim <lucas.furquim@sptech.school>
2024-12-07 13:32:47 -0300

    add notes component

--- snout-front/src/app/components/new-note.tsx
+++ snout-front/src/app/components/new-note.tsx
@@ -3,12 +3,12 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function NewNote(){
   return (
-    <div className="flex w-[900px] mt-16 p-10 h-full gap-10 shadow-md bg-foreground flex-col rounded-sm justify-center bg-zinc-200 bg-opacity-40">
+    <div className="flex w-[600px] mt-16 p-10 h-full gap-10 shadow-md bg-foreground flex-col rounded-sm justify-center bg-zinc-200 bg-opacity-40">
       <div className="flex w-full justify-start">
         <Textarea className="w-[400px] shadow-md resize-none bg-zinc-300" placeholder={"Titulo"}/>
       </div>
       <div className="flex w-full justify-center">
-        <Textarea className="w-full shadow-md resize-none bg-zinc-300" placeholder={"Conteudo"}/>
+        <Textarea className="w-full h-24 shadow-md resize-none bg-zinc-300" placeholder={"Conteudo"}/>
       </div>
       <div className="flex justify-end ">
       <Button className="w-28 h-10 bg-neutral-800">Anotar</Button>
--- snout-front/src/app/notes/layout.tsx
+++ snout-front/src/app/notes/layout.tsx
@@ -5,7 +5,7 @@ import NotesPage from "./page";
 
 export default function NotesLayout() {
   return (
-        <div className="flex justify-center flex-col items-center">
+        <div className="flex justify-center  flex-col items-center">
           <NavBar/>
           <NotesPage/>
         </div>
