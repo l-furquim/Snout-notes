ef345cb548b42e44c430f68fdc03893f6bbc87d4
l-furquim <lucas.furquim@sptech.school>
2024-12-07 13:54:38 -0300

    add reminders component

--- snout-front/src/app/components/nav-bar.tsx
+++ snout-front/src/app/components/nav-bar.tsx
@@ -9,41 +9,26 @@ export default function NavBar(){
     if(url == "/notes"){
       return (
         <>
-          <div className="w-full ml-20 flex justify-start">
-            <h3 className="hover:cursor-pointer hover:text-zinc-400">Snout Notes</h3>
-          </div>
-          <div className="w-full mr-20 gap-10 flex justify-end">
-            <h3 className="text-zinc-400 hover:cursor-pointer ">Notas</h3>
-            <h3 className="hover:cursor-pointer hover:text-zinc-400">Lembretes</h3>
-            <h3 className="hover:cursor-pointer hover:text-zinc-400">Perfil</h3>
-          </div>
+            <h3 onClick={() => location.replace("/notes")} className="text-zinc-400 hover:cursor-pointer ">Notas</h3>
+            <h3 onClick={() => location.replace("/reminder")} className="hover:cursor-pointer hover:text-zinc-400">Lembretes</h3>
+            <h3 onClick={() => location.replace("/profile")} className="hover:cursor-pointer hover:text-zinc-400">Perfil</h3>
         </>
       )
     }
     if(url == "/reminder"){
       return (
         <>
-            <div className="w-full ml-20 flex justify-start">
-              <h3 className="hover:cursor-pointer hover:text-zinc-400">Snout Notes</h3>
-            </div>
-            <div className="w-full mr-20 gap-10 flex justify-end">
-              <h3 className="hover:cursor-pointer hover:text-zinc-400">Notas</h3>
-              <h3 className="text-zinc-400 hover:cursor-pointer">Lembretes</h3>
-              <h3 className="hover:cursor-pointer hover:text-zinc-400">Perfil</h3>
-            </div>
+          <h3 onClick={() => location.replace("/notes")}  className="hover:cursor-pointer hover:text-zinc-400">Notas</h3>
+          <h3 onClick={() => location.replace("/reminder")} className="text-zinc-400 hover:cursor-pointer">Lembretes</h3>
+          <h3 onClick={() => location.replace("/profile")} className="hover:cursor-pointer hover:text-zinc-400">Perfil</h3>
           </>
       )
     }
     return (
       <>
-      <div className="w-full ml-20 flex justify-start">
-        <h3>Snout Notes</h3>
-      </div>
-      <div className="w-full mr-20 gap-10 flex justify-end">
-        <h3>Notas</h3>
-        <h3>Lembretes</h3>
-        <h3 className="text-zinc-400">Perfil</h3>
-      </div>
+        <h3 onClick={() => location.replace("/notes")} >Notas</h3>
+        <h3 onClick={() => location.replace("/reminder")}>Lembretes</h3>
+        <h3 onClick={() => location.replace("/profile")} className="text-zinc-400">Perfil</h3>
     </>
     )
   
@@ -51,7 +36,12 @@ export default function NavBar(){
   
   return (
     <div className="flex bg-neutral-800 w-full h-16 justify-center items-center font-bold text-zinc-200">
-      {getActualPage()}
+      <div className="w-full ml-20 flex justify-start">
+            <h3 className="hover:cursor-pointer hover:text-zinc-400">Snout Notes</h3>
+      </div>
+      <div className="w-full mr-20 gap-10 flex justify-end">
+        {getActualPage()}
+      </div>
     </div>
   )
 }
